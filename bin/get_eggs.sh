b4c107b2cef297bc2d7b0570c51dccbaf31278c0
TurtleKitty <joshua@anwu.org>
2017-06-29 00:36:43 -0500

    get_eggs

---
+++ bin/get_eggs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# You may need to run this script as root.
+
+chicken-install srfi-1 srfi-13 srfi-69 http-client medea numbers openssl posix symbol-utils unix-sockets utf8 utils uuid vector-lib
+
+
