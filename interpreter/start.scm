594cfa3c9e0b600e192d7bcb798ba5b550e9e9a0
TurtleKitty <joshua@anwu.org>
2019-03-07 00:08:30 -0600

    Factoring helper procs out of giant start proc

--- interpreter/start.scm
+++ interpreter/start.scm
@@ -1,19 +1,38 @@
 
+(define (vaquero-internal-prep-dir path)
+   (if (not (directory? path))
+      (create-directory path #t)
+      #f))
+
+(define (vaquero-internal-locate-path path)
+   (find-file (make-module-absolute-path path)))
+
+(define (vaquero-internal-start-run filename)
+   (define expanded (read-expand-cache-prog filename (cli-env)))
+   (define fpath (vaquero-internal-locate-path filename))
+   (define cpath (get-vaquero-compiled-path fpath))
+   (if (check-vaquero-syntax expanded)
+      (let ((is-cached (and (file-exists? cpath) (file-newer? cpath fpath))))
+         (if is-cached
+            (vaquero-run
+               (call-with-input-file
+                  cpath
+                  vaquero-read))
+            (let ((linked (vaquero-link expanded)) (save-file (open-output-file cpath)))
+               (vaquero-write linked save-file)
+               (close-output-port save-file)
+               (vaquero-run linked))))
+      (exit)))
+
 (define (start)
    (define args (command-line-arguments))
    (define (fname)
       (if (pair? (cdr args))
          (cadr args)
          (usage)))
-   (define (prep-dir path)
-      (if (not (directory? path))
-         (create-directory path #t)
-         #f))
-   (define (locate-path)
-      (find-file (make-module-absolute-path (fname))))
-   (prep-dir vaquero-mod-dir)
-   (prep-dir vaquero-expanded-dir)
-   (prep-dir vaquero-compiled-dir)
+   (vaquero-internal-prep-dir vaquero-mod-dir)
+   (vaquero-internal-prep-dir vaquero-expanded-dir)
+   (vaquero-internal-prep-dir vaquero-compiled-dir)
    (add-global-prelude (global-env))
    (if (not (pair? args))
       (usage)
@@ -31,20 +50,7 @@
                      (let ((linked (vaquero-link expanded)))
                        (vaquero-run linked))
                      (exit))))
-            ((run)
-               (let* ((expanded (read-expand-cache-prog (fname) (cli-env))) (fpath (locate-path)) (cpath (get-vaquero-compiled-path fpath)))
-                  (if (check-vaquero-syntax expanded)
-                     (let ((is-cached (and (file-exists? cpath) (file-newer? cpath fpath))))
-                        (if is-cached
-                           (vaquero-run
-                              (call-with-input-file
-                                 cpath
-                                 vaquero-read))
-                           (let ((linked (vaquero-link expanded)) (save-file (open-output-file cpath)))
-                              (vaquero-write linked save-file)
-                              (close-output-port save-file)
-                              (vaquero-run linked))))
-                     (exit))))
+            ((run) (vaquero-internal-start-run (fname)))
             ((check)
                (let ((its-good (check-vaquero-syntax (cdr (read-expand-cache-prog (fname) (cli-env))))))
                   (display "Syntax check complete: ")
@@ -63,7 +69,7 @@
             ((compile)
                (let ((expanded (read-expand-cache-prog (fname) (cli-env))))
                   (define linked (vaquero-link expanded))
-                  (define cpath (get-vaquero-compiled-path (locate-path)))
+                  (define cpath (get-vaquero-compiled-path (vaquero-internal-locate-path (fname))))
                   (define save-file (open-output-file cpath))
                   (vaquero-write linked save-file)
                   (close-output-port save-file)
@@ -74,5 +80,8 @@
                      (vaquero-view
                         (read-expand-cache-prog (fname) (cli-env))))
                   (newline)))
-            (else (printf "Unknown command: ~A~%" cmd))))))
+            (else
+               (if (pair? (cdr args))
+                  'do-something
+                  (else (printf "Unknown command: ~A~%" cmd))))))))
 
