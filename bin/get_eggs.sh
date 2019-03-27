9dfaa7445a48550c1e65bc6d8318ccbf1a0fbe54
TurtleKitty <joshua@anwu.org>
2019-03-27 00:38:45 -0500

    no need for uuid lib now

--- bin/get_eggs.sh
+++ bin/get_eggs.sh
@@ -2,6 +2,6 @@
 
 # You may need to run this script as root.
 
-chicken-install srfi-1 srfi-13 srfi-69 http-client openssl posix symbol-utils unix-sockets utf8 utils uuid vector-lib r7rs system-information posix-groups
+chicken-install srfi-1 srfi-13 srfi-69 http-client openssl symbol-utils unix-sockets utf8 utils vector-lib r7rs system-information posix-groups
 
 
