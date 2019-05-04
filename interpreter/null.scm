397dcdce1da40a9877bbdbd2d3182213dcaa2b85
TurtleKitty <joshua@anwu.org>
2019-05-03 19:38:44 -0500

    Made null explode like a little grenade on message-not-understood

--- interpreter/null.scm
+++ interpreter/null.scm
@@ -4,14 +4,8 @@
       (method answers?
          (cont (lambda (msg) (hte? vaquero-send-null-vtable msg))))
 
-      (method null-apply
-         (err (vaquero-error-object 'null-is-not-applicable '(null ...) "Null can not be used as a procedure.") cont))
-
       (method autos
          (cont '(autos messages to-bool to-text type view)))
-
-      (method default
-         (cont 'null))  ; FIXME make null blow up like a little grenade
 
       (method messages
          (cont (htks vaquero-send-null-vtable)))
@@ -30,12 +24,11 @@
 
       (alist->hash-table
          `((answers?   . ,answers?)
-           (apply      . ,null-apply)
            (autos      . ,autos)
            (messages   . ,messages)
            (to-bool    . ,to-bool)
            (to-text    . ,to-text)
            (type       . ,type)
            (view       . ,view)
-           (default    . ,default)))))
+           (default    . ,idk)))))
 
